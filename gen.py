b48f9d14599ee9bd1692f61788c4804d3faedab4
凌建发 <jianfa.ljf@alibaba-inc.com>
2021-01-16 13:53:56 +0800

    update

--- gen.py
+++ gen.py
@@ -29,99 +29,98 @@ def getdate(date):
     d = datetime.date.fromordinal(__s_date + date)
     return d
 
-def rrr_excel(rows):
-    z = []
-    for i in range(len(rows)):
+def rrr_xlsx_csv():
+    b = excel_read('rrr.xlsx')
+    c = []
+    for i in range(len(b)):
         if i > 0:
-            a = rows[i]
-            b = []
-            b.append(getdate(a[0]).strftime("%Y-%m-%d"))
-            b.append(getdate(a[1]).strftime("%Y-%m-%d"))
-            b.append(a[2])
-            b.append(a[3])
-            b.append(a[4])
-            b.append(a[5])
-            b.append(a[6])
-            b.append(a[7])
-            z.append(b)
-    return z
+            a = b[i]
+            t = []
+            t.append(getdate(a[0]).strftime("%Y-%m-%d"))
+            t.append(getdate(a[1]).strftime("%Y-%m-%d"))
+            t.append(a[2])
+            t.append(a[3])
+            t.append(a[4])
+            t.append(a[5])
+            t.append(a[6])
+            t.append(a[7])
+            c.append(t)
+    c.sort(key=lambda entry: entry[0], reverse=False)
+    d = []
+    for i in range(len(c)):
+        d.append(','.join(a if isinstance(a, str) else str(a) for a in c[i]))
+    with open('rrr.csv', 'w') as f:
+        f.write('\n'.join(d))
 
-def rrr_csv(rows):
-    z = []
-    for i in range(len(rows)):
-        if i > 0:
-            a = rows[i]
-            b = []
-            b.append(datetime.datetime.strptime(a[0].replace("年","-").replace("月","-").replace("日",""), '%Y-%m-%d').strftime("%Y-%m-%d"))
-            b.append(datetime.datetime.strptime(a[1].replace("年","-").replace("月","-").replace("日",""), '%Y-%m-%d').strftime("%Y-%m-%d"))
-            b.append(float(a[2].replace("%",""))/100)
-            b.append(float(a[3].replace("%",""))/100)
-            b.append(float(a[4].replace("%",""))/100)
-            b.append(float(a[5].replace("%",""))/100)
-            b.append(float(a[6].replace("%",""))/100)
-            b.append(float(a[7].replace("%",""))/100)
-            z.append(b)
-    return z
+def rrr_csv_json():
+    c = csv_read('rrr.csv')
+    d = []
+    for i in range(len(c)):
+        a = c[i]
+        t = [
+            '"公布时间":"{}"'.format(a[0]),
+            '"生效时间":"{}"'.format(a[1]),
+            '"大型金融机构":{}'.format(a[3]),
+            '"中小金融机构":{}'.format(a[6]),
+        ]
+        d.append('   {' + ','.join(t) + '}')
+    with open('rrr.json', 'w') as f:
+        f.write('[\n' + ',\n'.join(d) + '\n]')
+
+
+def mmm_xlsx_csv():
+    b = excel_read('mmm.xlsx')
+    c = []
+    for i in range(len(b)):
+        if i > 1:
+            a = b[i]
+            t = []
+            t.append(datetime.datetime.strptime(a[0].replace("年","-").replace("月份",""), '%Y-%m').strftime("%Y-%m"))
+            t.append(int(a[1]))
+            t.append(a[2])
+            t.append(a[3])
+            t.append(int(a[4]))
+            t.append(a[5])
+            t.append(a[6])
+            t.append(int(a[7]))
+            t.append(a[8])
+            t.append(a[9])
+            c.append(t)
+    c.sort(key=lambda entry: entry[0], reverse=False)
+    d = []
+    for i in range(len(c)):
+        d.append(','.join(a if isinstance(a, str) else str(a) for a in c[i]))
+    with open('mmm.csv', 'w') as f:
+        f.write('\n'.join(d))
+
+def mmm_csv_json():
+    c = csv_read('mmm.csv')
+    d = []
+    for i in range(len(c)):
+        a = c[i]
+        t = [
+            '"月份":"{}"'.format(a[0]),
+            '"M0亿元":{}'.format(a[1]),
+            '"M0同比增长":{}'.format(a[2]),
+            '"M0环比增长":{}'.format(a[3]),
+            '"M1亿元":{}'.format(a[4]),
+            '"M1同比增长":{}'.format(a[5]),
+            '"M1环比增长":{}'.format(a[6]),
+            '"M2亿元":{}'.format(a[7]),
+            '"M2同比增长":{}'.format(a[8]),
+            '"M2环比增长":{}'.format(a[9]),
+        ]
+        d.append('   {' + ','.join(t) + '}')
+    with open('mmm.json', 'w') as f:
+        f.write('[\n' + ',\n'.join(d) + '\n]')
 
 def rrr():
-    # z = rrr_csv(csv_read('rrr.csv'))
-    z = rrr_excel(excel_read('rrr.xlsx'))
-    z.sort(key=lambda entry: entry[0], reverse=False)
-    with open('rrr.json', 'w') as f:
-        f.write('[\n')
-        for j in range(len(z)):
-            a = z[j]
-            f.write('   {')
-            f.write('"公布时间":"{}",'.format(a[0]))
-            f.write('"生效时间":"{}",'.format(a[1]))
-            f.write('"大型金融机构":{:.3f},'.format(a[3]))
-            f.write('"中小金融机构":{:.3f}'.format(a[6]))
-            if j != len(z) - 1:
-                f.write('},\n')
-            else:
-                f.write('}\n')
-        f.write(']')
+    rrr_xlsx_csv()
+    rrr_csv_json()
 
 def mmm():
-    s = csv_read('mmm.csv')
-    z = []
-    for i in range(len(s)):
-        if i > 0:
-            a = s[i]
-            b = []
-            b.append(datetime.datetime.strptime(a[0].replace("年","-").replace("月份",""), '%Y-%m').strftime("%Y-%m"))
-            b.append(int(a[1]))
-            b.append(float(a[2].replace("%",""))/100)
-            b.append(float(a[3].replace("%",""))/100)
-            b.append(int(a[4]))
-            b.append(float(a[5].replace("%",""))/100)
-            b.append(float(a[6].replace("%",""))/100)
-            b.append(int(a[7]))
-            b.append(float(a[8].replace("%",""))/100)
-            b.append(float(a[9].replace("%",""))/100)
-            z.append(b)
-    z.sort(key=lambda entry: entry[0], reverse=False)
-    with open('mmm.json', 'w') as f:
-        f.write('[\n')
-        for j in range(len(z)):
-            a = z[j]
-            f.write('   {')
-            f.write('"月份":"{}",'.format(a[0]))
-            f.write('"M0亿元":{},'.format(a[1]))
-            f.write('"M0同比增长":{:.4f},'.format(a[2]))
-            f.write('"M0环比增长":{:.4f},'.format(a[3]))
-            f.write('"M1亿元":{},'.format(a[4]))
-            f.write('"M1同比增长":{:.4f},'.format(a[5]))
-            f.write('"M1环比增长":{:.4f},'.format(a[6]))
-            f.write('"M2亿元":{},'.format(a[7]))
-            f.write('"M2同比增长":{:.4f},'.format(a[8]))
-            f.write('"M2环比增长":{:.4f}'.format(a[9]))
-            if j != len(z) - 1:
-                f.write('},\n')
-            else:
-                f.write('}\n')
-        f.write(']')
-
+    mmm_xlsx_csv()
+    mmm_csv_json()
 
 rrr()
 mmm()
